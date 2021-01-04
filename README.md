cb0199b9b3093343fb72f7d07bc31fe7ba903fac
yingzhuo <yingzhor@gmail.com>
2021-01-04 13:17:56 +0800

    2021-01-04 13:17:56

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ```hiveql
 create function 
-desensitize as 'com.github.yingzhuo.hive.function.DesensitizeFunction' 
-using jar 'hdfs://192.168.99.130:8020/user/hive/jars/hive-my-functions-1.0.0.jar';
+    desensitize as 'com.github.yingzhuo.hive.function.DesensitizeFunction'
+    using jar 'hdfs://192.168.99.130:8020/user/hive/jars/hive-my-functions-1.0.0.jar';
+
+create function 
+    age as 'com.github.yingzhuo.hive.function.AgeFunction'
+    using jar 'hdfs://192.168.99.130:8020/user/hive/jars/hive-my-functions-1.0.0.jar';
 ```
---
+++ src/main/java/com/github/yingzhuo/hive/function/AgeFunction.java
@@ -0,0 +1,78 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ _   _ _             __  __         _____                 _   _
+| | | (_)_   _____  |  \/  |_   _  |  ___|   _ _ __   ___| |_(_) ___  _ __  ___
+| |_| | \ \ / / _ \ | |\/| | | | | | |_ | | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+|  _  | |\ V /  __/ | |  | | |_| | |  _|| |_| | | | | (__| |_| | (_) | | | \__ \
+|_| |_|_| \_/ \___| |_|  |_|\__, | |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+                            |___/
+ https://github.com/yingzhuo/hive-my-functions
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package com.github.yingzhuo.hive.function;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentTypeException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspector;
+
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ * 获取年龄
+ */
+@Description(name = "Age", value = "Calculate the Age of Given Column", extended = "SELECT age('1970-01-01', 'yyyy-MM-dd');")
+public class AgeFunction extends GenericUDF {
+
+    private StringObjectInspector dateObject;
+    private StringObjectInspector patternObject;
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
+
+        if (arguments.length != 2) {
+            throw new UDFArgumentLengthException("AgeFunction take 2 arguments.");
+        }
+
+        if (!(arguments[0] instanceof StringObjectInspector)) {
+            throw new UDFArgumentTypeException(0, "AgeFunction parameter (date) 0 need to string.");
+        }
+
+        if (!(arguments[1] instanceof StringObjectInspector)) {
+            throw new UDFArgumentTypeException(0, "AgeFunction parameter (pattern) 1 need to string.");
+        }
+
+        this.dateObject = (StringObjectInspector) arguments[0];
+        this.patternObject = (StringObjectInspector) arguments[1];
+
+        return PrimitiveObjectInspectorFactory.javaIntObjectInspector;
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[] arguments) throws HiveException {
+        try {
+            final String dateString = dateObject.getPrimitiveJavaObject(arguments[0].get());
+            final String patternString = patternObject.getPrimitiveJavaObject(arguments[1].get());
+
+            final Date now = new Date();
+            final Date date = DateUtils.parseDate(dateString, patternString);
+
+            long timeBetween = now.getTime() - date.getTime();
+            double yearsBetween = timeBetween / 3.15576e+10;
+            return (int) Math.floor(yearsBetween);
+        } catch (ParseException e) {
+            throw new HiveException(e.getMessage());
+        }
+    }
+
+    @Override
+    public String getDisplayString(String[] children) {
+        return null;
+    }
+
+}
